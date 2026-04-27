369e64d0ad7d0667d803245f86849bd118a69bf2
iamahencock-ui <iamahen.cock@gmail.com>
2026-04-27 12:05:23 -0700

    Update Combat_Engine.py

--- Combat_Engine.py
+++ Combat_Engine.py
@@ -4,4 +4,5 @@ combat_pages={'Light Attack':Combat_Page('Light Attack',2,[{'type':'slash','roll
               'Light Defense':Combat_Page('Light Defense',3,[{'type':'evade','rolls':[1,5]},{'type':'block','rolls':[2,3]},{'type':'slash','rolls':[1,2]}])}
 def clash(player,enemy):
     clashes=max(player.numdice,enemy.numdice)
-    for (int i=)
+    for i in range(clashes):
+        
