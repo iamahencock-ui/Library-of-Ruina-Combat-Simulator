6b39fa9e17c36f04497e9d9ca058f227947e9254
iamahencock-ui <iamahen.cock@gmail.com>
2026-04-28 11:18:23 -0700

    Update Combat_Engine.py

--- Combat_Engine.py
+++ Combat_Engine.py
@@ -3,6 +3,16 @@ from pages import *
 combat_pages={'Light Attack':Combat_Page('Light Attack',2,[{'type':'slash','rolls':[2,3]},{'type':'blunt','rolls':[1,4]}]),
               'Light Defense':Combat_Page('Light Defense',3,[{'type':'evade','rolls':[1,5]},{'type':'block','rolls':[2,3]},{'type':'slash','rolls':[1,2]}])}
 def clash(player,enemy):
+    """
+    Calculate the area of a circle.
+
+    Args:
+        player (combat_Page): The combat page the player uses
+        Enemy (combat_Page): The combat page the npc enemy uses
+
+    Returns:
+        list: The results of each individual clash inthe following format [{result:'player'/'enemy'/'tie',damage:int,status_infliction:{status name:int}}]
+    """
     clashes=max(player.numdice,enemy.numdice)
     result=[]
     for i in range(1,clashes+1):
@@ -15,4 +25,4 @@ def clash(player,enemy):
                 current='Player'
             else:
                 current='Enemy'
-            
+clash()
