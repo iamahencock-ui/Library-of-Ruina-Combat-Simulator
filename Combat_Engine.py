91bdd41063f504f8a87a0de348677b5f7799d4d6
iamahencock-ui <iamahen.cock@gmail.com>
2026-04-28 11:11:15 -0700

    Update Combat_Engine.py

--- Combat_Engine.py
+++ Combat_Engine.py
@@ -5,5 +5,14 @@ combat_pages={'Light Attack':Combat_Page('Light Attack',2,[{'type':'slash','roll
 def clash(player,enemy):
     clashes=max(player.numdice,enemy.numdice)
     result=[]
-    for i in range(clashes):
-        if ()
+    for i in range(1,clashes+1):
+        if player.numdice>=i and enemy.numdice>=i:
+            playerroll=random.randint(player.dice[i-1][0],player.dice[i-1][1])
+            enemyroll=random.randint(enemy.dice[i-1][0],enemy.dice[i-1][1])
+            if playerroll==enemyroll:
+                current='Tie'
+            elif playerroll>enemyroll:
+                current='Player'
+            else:
+                current='Enemy'
+            
